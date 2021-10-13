5569385c5ec5208905324b6ebb15878017a07cfd
Adrián Blanco <adrianblancocaballero@gmail.com>
2021-10-13 11:31:29 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+# Ejercicio de Clase 2
+numero = int(input("Introduzca un número: "))
+potencia = int(input("Introduzca la potencia a la que desea elevarlo: "))
+resultado = numero**potencia
+print("El resultado es:", resultado)
